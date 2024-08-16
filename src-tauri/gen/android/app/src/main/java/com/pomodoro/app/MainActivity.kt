67469a456e9f1ec370a8a65b73a201b40df058da
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-08-16 01:10:20 +0100

    initial commit

---
+++ src-tauri/gen/android/app/src/main/java/com/pomodoro/app/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.pomodoro.app
+
+class MainActivity : TauriActivity()
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+
+//styles
+import "./styles/tailwind.css";
+
+createApp(App).mount("#app");
