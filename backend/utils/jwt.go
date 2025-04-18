bc671c8931a2af392aa2a2ce61e055033e8b8844
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-04-18 23:40:04 +0100

    feat: move backend to rust

--- backend/utils/jwt.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "github.com/golang-jwt/jwt/v5"
-
-type JwtClaims struct {
-	Identifier string `json:"identifier"`
-	Email      string `json:"email"`
-	jwt.RegisteredClaims
-}
--- backend/utils/parser.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "strings"
-
-func ParseString(s string) string {
-	return strings.TrimSpace(strings.ToLower(s))
-}
